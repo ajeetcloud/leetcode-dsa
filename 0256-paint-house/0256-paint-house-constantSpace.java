eb471e76d7b8b2054e9b30318a06c6f8822560ec
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2024-05-05 19:42:08 +0530

    Time: 1 ms (Beats 88.09%), Space: 43.4 MB (Beats 10.45%)

---
+++ 0256-paint-house/0256-paint-house-constantSpace.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minCost(int[][] costs) {
+      int n = costs.length;
+      int[] prevRow = costs[0];
+      
+      // DP solution
+      int[][] storage = new int[n][3];
+      storage[0][0] = costs[0][0];
+      storage[0][1] = costs[0][1];
+      storage[0][2] = costs[0][2];
+      for (int i = 1; i < n; i++) {
+        
+        storage[i][0] = costs[i][0] + Math.min(storage[i - 1][1], storage[i - 1][2]);
+        storage[i][1] = costs[i][1] + Math.min(storage[i - 1][0], storage[i - 1][2]);
+        storage[i][2] = costs[i][2] + Math.min(storage[i - 1][0], storage[i - 1][1]);
+      }
+      return Math.min(storage[n - 1][0], Math.min(storage[n - 1][1], storage[n - 1][2]));
+    }
+}
