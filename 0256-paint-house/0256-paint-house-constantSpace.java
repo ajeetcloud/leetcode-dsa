82a23f6e21ebc6adfa6e45e8390b5f91cbd58504
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2024-05-05 19:43:56 +0530

    Time: 1 ms (Beats 88.09%), Space: 42.5 MB (Beats 79.66%)

--- 0256-paint-house/0256-paint-house-constantSpace.java
+++ 0256-paint-house/0256-paint-house-constantSpace.java
@@ -1,19 +1,17 @@
 class Solution {
     public int minCost(int[][] costs) {
       int n = costs.length;
-      int[] prevRow = costs[0];
-      
       // DP solution
-      int[][] storage = new int[n][3];
-      storage[0][0] = costs[0][0];
-      storage[0][1] = costs[0][1];
-      storage[0][2] = costs[0][2];
+      // Can be done using the costs matrix also, inplace, in this solution
+      // also we are ensure Space: 0(1), as prevRow and currentRow have constant sizes - 3
+      int[] prevRow = costs[0].clone();
       for (int i = 1; i < n; i++) {
-        
-        storage[i][0] = costs[i][0] + Math.min(storage[i - 1][1], storage[i - 1][2]);
-        storage[i][1] = costs[i][1] + Math.min(storage[i - 1][0], storage[i - 1][2]);
-        storage[i][2] = costs[i][2] + Math.min(storage[i - 1][0], storage[i - 1][1]);
+        int[] currentRow = costs[i].clone();
+        currentRow[0] = currentRow[0] + Math.min(prevRow[1], prevRow[2]);
+        currentRow[1] = currentRow[1] + Math.min(prevRow[0], prevRow[2]);
+        currentRow[2] = currentRow[2] + Math.min(prevRow[0], prevRow[1]);
+        prevRow = currentRow;
       }
-      return Math.min(storage[n - 1][0], Math.min(storage[n - 1][1], storage[n - 1][2]));
+      return Math.min(prevRow[0], Math.min(prevRow[1], prevRow[2]));
     }
 }
