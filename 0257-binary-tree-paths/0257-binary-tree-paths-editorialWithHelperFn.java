bbe901fafd3e29556dbec13398443465f8319398
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2024-05-06 22:10:44 +0530

    Time: 6 ms (Beats 64.07%), Space: 42.9 MB (Beats 10.69%)

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths-editorialWithHelperFn.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+     if (root == null) {
+      return null;
+     }
+     List<String> left = binaryTreePaths(root.left);
+     List<String> right = binaryTreePaths(root.right);
+     List<String> ans = new ArrayList<>();
+     String result = String.valueOf(root.val);
+     if (left != null) {
+      for (String smallAns: left) {
+        ans.add(result + "->" + smallAns);
+      }
+     }
+     if (right != null) {
+      for (String smallAns: right) {
+        ans.add(result + "->" + smallAns);
+      }
+     }
+     if (ans.isEmpty()){
+      ans.add(result);
+     }
+      return ans;
+    }
+  
+}
