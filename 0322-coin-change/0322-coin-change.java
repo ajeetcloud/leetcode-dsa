73bb6287f5895071c0445136d2f8a1de5d8167cc
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2024-05-10 22:30:43 +0530

    Time: 14 ms (Beats 87.6%), Space: 44 MB (Beats 85.01%)

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,9 +2,20 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int storage[] = new int[amount + 1];
         for (int i = 0; i < storage.length; i++) {
-          storage[i] = -1;
+          storage[i] = amount + 1;
         }
-        
+        storage[0] = 0;
+        for (int i = 1; i < storage.length; i++) {
+          for (int c = 0; c < coins.length; c++) {
+            int coin = coins[c];
+            if (i - coin < 0) {
+              continue;
+            }
+            storage[i] = Math.min(storage[i], storage[i - coin] + 1);
+          }
+          
+        }
+        return storage[amount] == amount + 1 ? -1 : storage[amount];
 
     }
 }
