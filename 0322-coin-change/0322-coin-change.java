ca76696b56f9848b7cb493aaac002a96ff944b22
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2024-05-10 22:29:10 +0530

    Time: N/A (Beats 0%), Space: N/A (Beats 0%)

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int storage[] = new int[amount + 1];
+        for (int i = 0; i < storage.length; i++) {
+          storage[i] = -1;
+        }
+        
+
+    }
+}
