6fe7c4a0921ccd97a65e60c2802b9a860cd7fe1f
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2025-03-06 08:14:27 +0530

    Time: 8 ms (Beats 84.56%), Space: 45.1 MB (Beats 42.65%)

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -7,15 +7,17 @@ class Solution {
     int result = 0;
     int maxFreq = 0;
     int windowLength = 0;
+    int currentChar = 0;
+    int leavingChar = 0;
     while (end <= lastIndex) {
-      int currentChar = s.charAt(end) - 'A';
+      currentChar = s.charAt(end) - 'A';
       freqMap[currentChar]++;
       maxFreq = Math.max(maxFreq, freqMap[currentChar]);
       windowLength = end - start + 1;
       if (windowLength - maxFreq <= k) {
         result = Math.max(result, windowLength);
       } else {
-        int leavingChar = s.charAt(start) - 'A';
+        leavingChar = s.charAt(start) - 'A';
         freqMap[leavingChar]--;
         start++;
       }
