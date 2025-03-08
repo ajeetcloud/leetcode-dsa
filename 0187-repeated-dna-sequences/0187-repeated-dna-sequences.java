fd8c7a859e9166e8e6c8ad135f6ae4fd4753d812
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2025-03-08 19:55:04 +0530

    Time: 31 ms (Beats 10.31%), Space: 48.9 MB (Beats 95.89%)

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,39 +1,44 @@
 class Solution {
+    public static List<String> findRepeatedDnaSequences(String s) {
 
-    public List<String> findRepeatedDnaSequences(String s) {
-        int L = 10, n = s.length();
-        if (n <= L) return new ArrayList();
-
-        // rolling hash parameters: base a
-        int a = 4, aL = (int) Math.pow(a, L);
-
-        // convert string to array of integers
-        Map<Character, Integer> toInt = new HashMap() {
-            {
-                put('A', 0);
-                put('C', 1);
-                put('G', 2);
-                put('T', 3);
-            }
-        };
-        int[] nums = new int[n];
-        for (int i = 0; i < n; ++i) nums[i] = toInt.get(s.charAt(i));
-
+        int k = 10;
+        int length = s.length();
+        if (k >= length) {
+            return new ArrayList<>();
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
+        int a = 4;
+        int start = 0;
+        int end = start + k - 1;
         int h = 0;
-        Set<Integer> seen = new HashSet();
-        Set<String> output = new HashSet();
-        // iterate over all sequences of length L
-        for (int start = 0; start < n - L + 1; ++start) {
-            // compute hash of the current sequence in O(1) time
-            if (start != 0) h =
-                h * a - nums[start - 1] * aL + nums[start + L - 1];
-            // compute hash of the first sequence in O(L) time
-            else for (int i = 0; i < L; ++i) h = h * a + nums[i];
+        int p = k - 1;
+        // calculate initial hash
+        for (int i = 0; i <= end; i++) {
+            char c = s.charAt(i);
+            h += map.get(c) * (Math.pow(a, p));
+            p--;
+        }
+        Set<Integer> seen = new HashSet<>();
+        Set<String> results = new HashSet<>();
+        seen.add(h);
 
-            // update output and hashset of seen sequences
-            if (seen.contains(h)) output.add(s.substring(start, start + L));
+        while (end <= length - 2) {
+            char prevC = s.charAt(start);
+            start++;
+            end++;
+            h = h - ((map.get(prevC)) * (int)(Math.pow(a, k - 1)));
+            h *= a;
+            char endC = s.charAt(end);
+            h += ((map.get(endC)) * (int)(Math.pow(a, 0)));
+            if (seen.contains(h)) {
+                results.add(s.substring(start, start + k));
+            }
             seen.add(h);
         }
-        return new ArrayList<String>(output);
+        return new ArrayList<>(results);
     }
 }
