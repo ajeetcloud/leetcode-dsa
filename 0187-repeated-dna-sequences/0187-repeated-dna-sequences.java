09cb1f78e0afb07cdd363de758b6b6bc85df81b8
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2025-03-08 19:52:20 +0530

    Time: 16 ms (Beats 86.1%), Space: 51.4 MB (Beats 88.67%)

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,24 +1,39 @@
 class Solution {
+
     public List<String> findRepeatedDnaSequences(String s) {
-      Set<String> results = new HashSet<>();
-      int k = 10;
-      int start = 0;
-      int end = start + k;
-      int lastIndex = s.length() - 1;
-      Set<String> subStrSet = new HashSet<>();
-      while (end <= lastIndex) {
-        String subStr = s.substring(start, end);
-        if (subStrSet.contains(subStr)) {
-          results.add(subStr);
-        } else {
-          subStrSet.add(subStr);
+        int L = 10, n = s.length();
+        if (n <= L) return new ArrayList();
+
+        // rolling hash parameters: base a
+        int a = 4, aL = (int) Math.pow(a, L);
+
+        // convert string to array of integers
+        Map<Character, Integer> toInt = new HashMap() {
+            {
+                put('A', 0);
+                put('C', 1);
+                put('G', 2);
+                put('T', 3);
+            }
+        };
+        int[] nums = new int[n];
+        for (int i = 0; i < n; ++i) nums[i] = toInt.get(s.charAt(i));
+
+        int h = 0;
+        Set<Integer> seen = new HashSet();
+        Set<String> output = new HashSet();
+        // iterate over all sequences of length L
+        for (int start = 0; start < n - L + 1; ++start) {
+            // compute hash of the current sequence in O(1) time
+            if (start != 0) h =
+                h * a - nums[start - 1] * aL + nums[start + L - 1];
+            // compute hash of the first sequence in O(L) time
+            else for (int i = 0; i < L; ++i) h = h * a + nums[i];
+
+            // update output and hashset of seen sequences
+            if (seen.contains(h)) output.add(s.substring(start, start + L));
+            seen.add(h);
         }
-        start++;
-        end++;
-      }
-
-
-
-      return results.stream().toList();
+        return new ArrayList<String>(output);
     }
 }
