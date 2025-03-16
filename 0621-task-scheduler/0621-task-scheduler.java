9e48d29aee3506b0c47013871735bdb702d24493
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2025-03-16 08:03:58 +0530

    Time: 2 ms (Beats 99.1%), Space: 46.3 MB (Beats 18.69%)

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+      int freq[] = new int[26];
+      for (int i = 0; i < tasks.length; i++) {
+        freq[tasks[i] - 'A']++;
+      }
+      Arrays.sort(freq);
+      int gaps = freq[25] - 1;
+      int idleSlots = gaps * n;
+      for (int i = 24; i >= 0 && freq[i] > 0; i--) {
+        idleSlots = idleSlots - Math.min(gaps, freq[i]);
+        if (idleSlots < 0) {
+          idleSlots = 0;
+          break;
+        }
+      }
+      return tasks.length + idleSlots;  
+    }
+}
