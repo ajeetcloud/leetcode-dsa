d606a76464c96467647b82860bb5b942200f909f
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2025-05-20 23:29:08 +0530

    Time: 8 ms (Beats 66.37%), Space: 46.3 MB (Beats 38.94%)

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,6 +1,23 @@
 class Solution {
-    // Greedy
+
     public int minMeetingRooms(int[][] intervals) {
+      Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+      PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+      int i = 0;
+      while (i < intervals.length) {
+        int startTime = intervals[i][0];
+        int endTime = intervals[i][1];
+        if (!minHeap.isEmpty() && startTime >= minHeap.peek()) {
+          minHeap.poll();
+        }
+        minHeap.offer(endTime);
+        i++;
+      }
+      return minHeap.size();
+    }
+
+    // Greedy
+    public int minMeetingRoomsGreedy(int[][] intervals) {
         int[]startTimes = new int[intervals.length];
         int[]endTimes = new int[intervals.length];
         for (int i = 0; i < intervals.length; i++) {
