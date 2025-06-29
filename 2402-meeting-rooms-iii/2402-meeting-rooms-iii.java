d7aa68d231c79367eb5ba18677776c3474b08a90
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2025-06-29 20:56:55 +0530

    Time: N/A (Beats 0%), Space: N/A (Beats 0%)

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+
+        PriorityQueue<Integer> freeRooms = new PriorityQueue<>(); // stores room numbers
+        PriorityQueue<int[]> occupiedRooms = new PriorityQueue<>((a,b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]); // stores availability(end) time, room number
+        int[] meetingCount = new int[n];
+        Arrays.sort(meetings, (a,b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        for (int i = 0; i < n; i++) {
+            freeRooms.offer(i);
+        }
+        for (int[] meeting: meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            // update occupiedRooms & freeRooms
+            while (!occupiedRooms.isEmpty() && occupiedRooms.peek()[0] < start) {
+                int roomNumber = (int) occupiedRooms.poll()[1]; // roomNumber
+                freeRooms.offer(roomNumber);
+            }
+            if (!freeRooms.isEmpty()) {
+                int roomNumber = freeRooms.poll();
+                occupiedRooms.offer(new int[]{end, roomNumber});
+                meetingCount[roomNumber]++;
+            } else {
+                int[] takeout = occupiedRooms.poll();
+                int takeoutEndtime = takeout[0];
+                int takeoutRoom = takeout[1];
+
+                int newEndtime = takeoutEndtime + (end - start);
+                occupiedRooms.offer(new int[]{newEndtime, takeoutRoom});
+                meetingCount[takeoutRoom]++;
+            }
+        }
+
+        int maxCount = 0;
+        int resultRoom = 0;
+        for (int i = 0; i < meetingCount.length; i++) {
+            if (meetingCount[i] > maxCount) {
+                maxCount = meetingCount[i];
+                resultRoom = i;
+            }
+        }
+        return resultRoom;
+    }
+}
