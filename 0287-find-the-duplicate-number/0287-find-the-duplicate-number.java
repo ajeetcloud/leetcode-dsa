0ab1cff2fd67eb57642fe71dd9f227f20b9a6daa
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2025-12-23 22:08:50 +0530

    Time: 5 ms (Beats 60.93%), Space: 82.9 MB (Beats 45.86%)

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,18 +1,19 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
         do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
 
-        fast = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        return slow;
+        return hare;
     }
 }
