b3773fbbde4714b03741680cb1442d4f55408e82
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-01 20:34:01 +0530

    Time: 24 ms (Beats 31.76%), Space: 51.1 MB (Beats 93.51%)

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,44 +1,49 @@
 class Solution {
-    public static List<String> findRepeatedDnaSequences(String s) {
-
-        int k = 10;
-        int length = s.length();
-        if (k >= length) {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> result = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+        int start = 0;
+        int end = 9;
+        int sum = 0;
+        if (s.length() < 10) {
             return new ArrayList<>();
         }
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('A', 0);
-        map.put('C', 1);
-        map.put('G', 2);
-        map.put('T', 3);
-        int a = 4;
-        int start = 0;
-        int end = start + k - 1;
-        int h = 0;
-        int p = k - 1;
-        // calculate initial hash
-        for (int i = 0; i <= end; i++) {
+        // calculate the sum for 1st window
+        int power = 9;
+        for (int i = start; i <= end; i++) {
             char c = s.charAt(i);
-            h += map.get(c) * (Math.pow(a, p));
-            p--;
+            sum += getNum(c) * Math.pow(4, power);
+            power--;
         }
-        Set<Integer> seen = new HashSet<>();
-        Set<String> results = new HashSet<>();
-        seen.add(h);
-
-        while (end <= length - 2) {
-            char prevC = s.charAt(start);
+        set.add(sum);
+        start = 0;
+        for (end = 10; end < s.length(); end++) {
+            char leavingChar = s.charAt(start);
             start++;
-            end++;
-            h = h - ((map.get(prevC)) * (int)(Math.pow(a, k - 1)));
-            h *= a;
-            char endC = s.charAt(end);
-            h += ((map.get(endC)) * (int)(Math.pow(a, 0)));
-            if (seen.contains(h)) {
-                results.add(s.substring(start, start + k));
+            int leavingNum = getNum(leavingChar);
+            sum = sum - (leavingNum * (int)Math.pow(4, 9));
+            sum = sum * 4;
+            char incomingChar = s.charAt(end);
+            int incomingNum = getNum(incomingChar);
+            sum = sum + incomingNum;
+            if (set.contains(sum)) {
+                String s1 = s.substring(start, start + 10);
+                result.add(s1);
             }
-            seen.add(h);
+            set.add(sum);
         }
-        return new ArrayList<>(results);
+        return new ArrayList<>(result);
+    }
+
+    public int getNum(char c) {
+        if (c == 'A') {
+            return 0;
+        } else if (c == 'C') {
+            return 1;
+        } else if (c == 'G') {
+            return 2;
+        } else {
+            return 3;
+        }
     }
 }
