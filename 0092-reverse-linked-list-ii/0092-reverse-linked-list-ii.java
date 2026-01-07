978d170abc3fb97d2868ddbf48cb63b467128f97
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-07 16:34:31 +0530

    Time: 0 ms (Beats 100%), Space: 42.9 MB (Beats 67.8%)

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,42 +10,47 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-      int count = 0;
-      ListNode tempLeft = null;
-      ListNode c1 = null;
-      ListNode temp = head;
-      if (left == right) {
-        return head;
-      }
-      boolean normalCase = true;
-      if (left >= 2) {
-        while (count < left - 2) {
-          temp = temp.next;
-          count++;
+        if (head == null || head.next == null) {
+            return head;
         }
-        c1 = temp.next;
-        tempLeft = c1;
-      } else {
-        c1 = temp;
-        tempLeft = head;
-        normalCase = false;
-      }
-      // System.out.println(temp.val);
-      // temp is before reverse window
-      count = 0;
-      ListNode c2 = c1.next;
-      ListNode c3 = null;
-      while (c2 != null && count < (right - left)) {
-        c3 = c2.next;
-        c2.next = c1;
-        c1 = c2;
-        c2 = c3;
-        count++;
-      }
+        if (left == right) {
+            return head;
+        }
+        // Arrive at left - starting point
+        ListNode temp = head;
+        ListNode leftNode = null;
+        ListNode prevLeftNode = null;
+        int count = left - 1;
+        while (count > 0) {
+            prevLeftNode = temp;
+            temp = temp.next;
+            count--;
+        }
+        if (prevLeftNode == null) {
+            leftNode = head;
+        } else {
+            leftNode = prevLeftNode.next;
+        } 
+        // Reverse the direction
+        ListNode c1 = leftNode;
+        ListNode c2 = c1.next;
+        ListNode c3 = null;
+        count = right - left;
+        while (count > 0 && c2 != null) {
+            c3 = c2.next;
+            c2.next = c1;
 
-      temp.next = c1;
-      tempLeft.next = c2;
+            c1 = c2;
+            c2 = c3;
 
-      return normalCase ? head : c1;
+            count--;
+        }
+        leftNode.next = c3;
+        if (prevLeftNode == null) {
+            return c1;
+        } else {
+            prevLeftNode.next = c1;
+        }
+        return head;
     }
 }
