d19b30725a533b701520c02fe6d526943c7c7620
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-12 17:25:30 +0530

    Time: 8 ms (Beats 93.96%), Space: 48.5 MB (Beats 39.8%)

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,45 +1,22 @@
 class Solution {
-
-    public int minMeetingRoomsMinHeap(int[][] intervals) {
-      Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-      PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-      int i = 0;
-      while (i < intervals.length) {
-        int startTime = intervals[i][0];
-        int endTime = intervals[i][1];
-        if (!minHeap.isEmpty() && startTime >= minHeap.peek()) {
-          minHeap.poll();
-        }
-        minHeap.offer(endTime);
-        i++;
-      }
-      return minHeap.size();
-    }
-
-    // Greedy
     public int minMeetingRooms(int[][] intervals) {
-        int[]startTimes = new int[intervals.length];
-        int[]endTimes = new int[intervals.length];
-        for (int i = 0; i < intervals.length; i++) {
-          startTimes[i] = intervals[i][0];
-          endTimes[i] = intervals[i][1];
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // for storing endtime
+
+        int result = 1;
+        minHeap.offer(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] currentInterval = intervals[i];
+            int minEndTimeHeap = minHeap.peek();
+            int currentStartTime =  currentInterval[0];
+            if (minEndTimeHeap > currentStartTime) {
+                result++;
+            } else {
+                minHeap.poll();
+            }
+            minHeap.offer(currentInterval[1]);
         }
-        Arrays.sort(startTimes);
-        Arrays.sort(endTimes);
-        int i = 0;
-        int j = 0;
-        int max = 0;
-        int count = 0;
-        while (i < startTimes.length && j < startTimes.length) {
-          if (startTimes[i] < endTimes[j]) {
-            count++;
-            max = Math.max(max, count);
-            i++;
-          } else {
-            count--;
-            j++;
-          }
-        }
-      return max;
+        return result;
     }
 }
