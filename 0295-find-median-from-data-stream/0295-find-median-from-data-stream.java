7daa12be4849ce827cf8bc77c0a1ce85ba62aca7
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-15 23:24:51 +0530

    Time: 134 ms (Beats 63.69%), Space: 111.7 MB (Beats 29.48%)

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,40 +1,59 @@
 class MedianFinder {
 
-  PriorityQueue<Integer> pqMinHeap;
-  PriorityQueue<Integer> pqMaxHeap;
+    PriorityQueue<Integer> maxHeap; // 1st Half
+    PriorityQueue<Integer> minHeap; // 2nd Half
+    int s1;
+    int s2;
 
-  public MedianFinder() {
-    pqMinHeap = new PriorityQueue<>();
-    pqMaxHeap = new PriorityQueue<>(Collections.reverseOrder());
-  }
-
-  public void addNum(int num) {
-    if (pqMaxHeap.isEmpty() || num <= pqMaxHeap.peek()) {
-      pqMaxHeap.offer(num);
-    } else {
-      pqMinHeap.offer(num);
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+        s1 = maxHeap.size();
+        s2 = minHeap.size();
     }
-    // Balancing step
-    if (pqMaxHeap.size() > pqMinHeap.size() + 1) {
-      pqMinHeap.offer(pqMaxHeap.poll());
-    } else if (pqMinHeap.size() > pqMaxHeap.size() + 1) {
-      pqMaxHeap.offer(pqMinHeap.poll());
+    
+    public void addNum(int num) {
+        s1 = maxHeap.size();
+        s2 = minHeap.size();
+        if (s1 == 0 && s2 == 0) {
+            maxHeap.offer(num);
+            return;
+        }
+
+        int firstHalfMax = maxHeap.peek();
+        if (num >= firstHalfMax) {
+            minHeap.offer(num);
+        } else {
+            maxHeap.offer(num);
+        }
+        if (Math.abs(maxHeap.size() - minHeap.size()) > 1) {
+            balance();
+        }
     }
 
-  }
-
-  public double findMedian() {
-    int minSize = pqMinHeap.size();
-    int maxSize = pqMaxHeap.size();
-    if (maxSize > minSize) {
-      return (double) pqMaxHeap.peek();
-    } else if (minSize > maxSize) {
-      return (double) pqMinHeap.peek();
-    } else if (maxSize > 0 && minSize > 0 && maxSize == minSize) {
-      return (double) (pqMaxHeap.peek() + pqMinHeap.peek()) / 2;
+    public void balance() {
+        s1 = maxHeap.size(); 
+        s2 = minHeap.size();
+        if (s1 - s2 > 1) {
+            int max = maxHeap.poll();
+            minHeap.offer(max);
+        } else if (s2 - s1 > 1) {
+            int min = minHeap.poll();
+            maxHeap.offer(min);
+        }
     }
-    return 0d;
-  }
+    
+    public double findMedian() {
+        s1 = maxHeap.size(); 
+        s2 = minHeap.size();
+        if (s1 == s2) {
+            return (double)(maxHeap.peek() + minHeap.peek()) / 2;
+        } else if (s1 > s2) {
+            return maxHeap.peek();
+        } else {
+            return minHeap.peek();
+        }
+    }
 }
 
 /**
