dc6d27a9b0200f19028aabd398ca8a5e2e4ede5a
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-15 23:51:22 +0530

    Time: 126 ms (Beats 81.08%), Space: 111.2 MB (Beats 88.1%)

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -2,26 +2,14 @@ class MedianFinder {
 
     PriorityQueue<Integer> maxHeap; // 1st Half
     PriorityQueue<Integer> minHeap; // 2nd Half
-    int s1;
-    int s2;
 
     public MedianFinder() {
         maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         minHeap = new PriorityQueue<>();
-        s1 = maxHeap.size();
-        s2 = minHeap.size();
     }
     
     public void addNum(int num) {
-        s1 = maxHeap.size();
-        s2 = minHeap.size();
-        if (s1 == 0 && s2 == 0) {
-            maxHeap.offer(num);
-            return;
-        }
-
-        int firstHalfMax = maxHeap.peek();
-        if (num >= firstHalfMax) {
+        if (maxHeap.isEmpty() || num >= maxHeap.peek()) {
             minHeap.offer(num);
         } else {
             maxHeap.offer(num);
@@ -32,20 +20,18 @@ class MedianFinder {
     }
 
     public void balance() {
-        s1 = maxHeap.size(); 
-        s2 = minHeap.size();
+        int s1 = maxHeap.size(); 
+        int s2 = minHeap.size();
         if (s1 - s2 > 1) {
-            int max = maxHeap.poll();
-            minHeap.offer(max);
+            minHeap.offer(maxHeap.poll());
         } else if (s2 - s1 > 1) {
-            int min = minHeap.poll();
-            maxHeap.offer(min);
+            maxHeap.offer(minHeap.poll());
         }
     }
     
     public double findMedian() {
-        s1 = maxHeap.size(); 
-        s2 = minHeap.size();
+        int s1 = maxHeap.size(); 
+        int s2 = minHeap.size();
         if (s1 == s2) {
             return (double)(maxHeap.peek() + minHeap.peek()) / 2;
         } else if (s1 > s2) {
