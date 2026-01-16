fc10fa1d879ffd73923b5aee2932a84562b4a24e
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-16 22:12:03 +0530

    Time: 76 ms (Beats 16.82%), Space: 146 MB (Beats 5.19%)

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -12,8 +12,10 @@ class RoomInfo {
 
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
+
         int[] freqMap = new int[n];
         Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+
         PriorityQueue<Integer> availableRooms = new PriorityQueue<>();
         PriorityQueue<RoomInfo> usedRooms = new PriorityQueue<>(
             (a, b) -> a.meetingEndtime != b.meetingEndtime 
@@ -26,25 +28,25 @@ class Solution {
         for (int i = 0; i < meetings.length; i++) {
             long currentStart = meetings[i][0];
             long currentEnd = meetings[i][1];
+
             // freeing up used rooms is important
             while (!usedRooms.isEmpty() && currentStart >= usedRooms.peek().meetingEndtime) {
                 RoomInfo roomToBeMadeAvailable = usedRooms.poll();
                 availableRooms.offer(roomToBeMadeAvailable.roomNumber);
             }
+
             if (!availableRooms.isEmpty()) {
                 int roomNo = availableRooms.poll();
                 usedRooms.offer(new RoomInfo(currentEnd, roomNo));
                 freqMap[roomNo]++;
             } else {
                 // check for earliest available room
-                if (!usedRooms.isEmpty()) {
-                    RoomInfo nextAvailableRoom = usedRooms.poll();
-                    int nextAvailableRoomNo = nextAvailableRoom.roomNumber;
-                    long nextMeetingEndtime = nextAvailableRoom.meetingEndtime;
-                    RoomInfo currentMeeting = new RoomInfo(nextMeetingEndtime + currentEnd - currentStart, nextAvailableRoomNo);
-                    usedRooms.offer(currentMeeting);
-                    freqMap[nextAvailableRoomNo]++;
-                }
+                RoomInfo nextAvailableRoom = usedRooms.poll();
+                int nextAvailableRoomNo = nextAvailableRoom.roomNumber;
+                long nextMeetingEndtime = nextAvailableRoom.meetingEndtime;
+                RoomInfo currentMeeting = new RoomInfo(nextMeetingEndtime + currentEnd - currentStart, nextAvailableRoomNo);
+                usedRooms.offer(currentMeeting);
+                freqMap[nextAvailableRoomNo]++;
             }
         }
         int maxMeetings = 0;
