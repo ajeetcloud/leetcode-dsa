ca11c27ecd3543a706de2acfbac671e002236643
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-17 16:21:24 +0530

    Time: 61 ms (Beats 23.1%), Space: 119.9 MB (Beats 80.32%)

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -4,11 +4,11 @@ class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
 
         List<List<Integer>> result = new ArrayList<>();
-        Set<String> seen = new HashSet<>();
+        Set<Pair> seen = new HashSet<>();
         PriorityQueue<Triplet> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.sum(), b.sum()));
         Triplet firstAnswer = new Triplet(nums1[0] + nums2[0], 0 , 0);
         pq.offer(firstAnswer);
-        seen.add("0,0");
+        seen.add(new Pair<Integer, Integer>(0, 0));
         int count = 0;
         while (!pq.isEmpty() && count < k) {
             Triplet takeout = pq.poll();
@@ -18,15 +18,18 @@ class Solution {
 
             result.add(Arrays.asList(nums1[i], nums2[j]));
 
-            if (j + 1 < nums2.length && !seen.contains(i + "," + (j + 1))) {
+            Pair<Integer, Integer> pair1 = new Pair<>(i, j + 1);
+            if (j + 1 < nums2.length && !seen.contains(pair1)) {
                 Triplet ans1 = new Triplet(nums1[i] + nums2[j + 1], i, j + 1);
                 pq.offer(ans1); 
-                seen.add(i + "," + (j + 1)); 
+                seen.add(pair1); 
             }     
-            if (i + 1 < nums1.length && !seen.contains((i + 1) + "," + j)) {
+
+            Pair<Integer, Integer> pair2 = new Pair<>(i + 1, j);
+            if (i + 1 < nums1.length && !seen.contains(pair2)) {
                 Triplet ans2 = new Triplet(nums1[i + 1] + nums2[j], i + 1, j);
                 pq.offer(ans2);
-                seen.add((i + 1) + "," + j);     
+                seen.add(pair2);     
             } 
         }
         return result;
