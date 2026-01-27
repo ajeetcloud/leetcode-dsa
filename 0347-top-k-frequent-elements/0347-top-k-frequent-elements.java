4389567231ca34eac5ef1c97358cd56915f27de1
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-01-28 00:10:58 +0530

    Time: 11 ms (Beats 94.67%), Space: 52.1 MB (Beats 5.34%)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,5 +1,33 @@
 class Solution {
+
+    // Bucket Sort
     public int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num: nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+        List<Integer>[] freqBuckets = new List[nums.length + 1];
+        for (int i = 0; i < freqBuckets.length; i++) {
+            freqBuckets[i] = new ArrayList<>();
+        } 
+        for (Map.Entry<Integer, Integer> entry: freqMap.entrySet()) {
+            freqBuckets[entry.getValue()].add(entry.getKey()); 
+        }
+        List<Integer> flattened = new ArrayList<>();
+        for (int i = freqBuckets.length - 1; i >= 0; i--) {
+            for (int n: freqBuckets[i]) {
+                flattened.add(n);
+            }
+        }
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = flattened.get(i);
+        }
+        return result;
+    }
+
+    public int[] topKFrequent1(int[] nums, int k) {
 
         if (k == nums.length) {
             return nums;
