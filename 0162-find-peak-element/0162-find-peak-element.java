c9a691a992fe222da9c8c6f170fba96607c281ae
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-02-12 19:54:21 +0530

    Time: 0 ms (Beats 100%), Space: 44.2 MB (Beats 73.03%)

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,23 +1,11 @@
-class Solution {
+public class Solution {
     public int findPeakElement(int[] nums) {
-        
-        int startIndex = 0;
-        int endIndex = nums.length - 1;
-
-        while (startIndex <= endIndex) {
-            int mid = startIndex + (endIndex - startIndex) / 2;
-            boolean isLeftSmaller = (mid == 0) || (nums[mid] > nums[mid - 1]);
-            boolean isRightSmaller = (mid == nums.length - 1) || (nums[mid] > nums[mid + 1]);
-            if (isLeftSmaller && isRightSmaller) {
-                return mid;
-            } 
-            else if (nums[mid + 1] > nums[mid]) {
-                startIndex = mid + 1;
-            }
-            else {
-                endIndex = mid - 1;
-            }
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1]) r = mid;
+            else l = mid + 1;
         }
-        return -1;
+        return l;
     }
 }
