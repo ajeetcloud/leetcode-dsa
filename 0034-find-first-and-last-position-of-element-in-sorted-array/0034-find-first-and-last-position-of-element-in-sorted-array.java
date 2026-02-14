c99e2b66a2864f85db3594aa14eb2b71ab543fc9
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-02-15 00:04:53 +0530

    Time: 0 ms (Beats 100%), Space: 48.3 MB (Beats 27.77%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -5,14 +5,43 @@ class Solution {
             return new int[]{-1, -1};
         }
 
-        return new int[]{ getBoundary(nums, target, true), getBoundary(nums, target, false) };
+        return new int[]{ getOptimisedIndex(nums, target, true), getOptimisedIndex(nums, target, false) };
     }
 
     private int getOptimisedIndex(int[] nums, int target, boolean findFirst) {
 
+        int startIndex = 0;
+        int endIndex = nums.length - 1;
+
+        while (startIndex <= endIndex) {
+
+            int mid = startIndex + (endIndex - startIndex ) / 2;
+            if (nums[mid] == target) {
+
+                if (findFirst) {
+                    // Finds first occurrence
+                    if (mid == 0 || nums[mid - 1] != target) {
+                        return mid;
+                    }
+                    endIndex = mid - 1;
+                }
+                else {
+                    // Finds last occurrence
+                    if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                        return mid;
+                    }
+                    startIndex = mid + 1;
+                }
+            } 
+            else if (nums[mid] > target) {
+                endIndex = mid - 1;
+            }
+            else {
+                startIndex = mid + 1;
+            }
+        }
         return -1;
     }
-
 
     private int getBoundary(int[] nums, int target, boolean findFirst) {
 
