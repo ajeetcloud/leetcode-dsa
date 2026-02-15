9c1d5bb680dcbc7861d5edaec26d9b845157a2c6
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-02-15 12:06:15 +0530

    Time: 4 ms (Beats 98.2%), Space: 47.9 MB (Beats 64.67%)

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,5 +1,31 @@
 class Solution {
+
+    // Binary Search To Find The Left Bound of the Window - more Optimal
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+        List<Integer> result = new ArrayList<>();
+
+        int startIndex = 0;
+        int endIndex = arr.length - k - 1;
+
+        while (startIndex <= endIndex) {
+            int mid = startIndex + (endIndex - startIndex) / 2;
+
+            if (x - arr[mid] <= arr[mid + k] - x) {
+                endIndex = mid - 1; // try better case, use startIndex
+            } 
+            else {
+                startIndex = mid + 1;
+            }
+        }
+        for (int i = startIndex; i < startIndex + k; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+
+    // Binary Search + Sliding Window
+    public List<Integer> findClosestElements1(int[] arr, int k, int x) {
 
         List<Integer> result = new ArrayList<>();
 
@@ -17,6 +43,7 @@ class Solution {
 
         int i = closestIndex - 1;
         int j = closestIndex;
+        // Sliding window solution
         while (j - i - 1 < k) {
             if (i < 0) {
                 j++;
