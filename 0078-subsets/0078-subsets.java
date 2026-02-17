4e89f3004e517e371fcf5a14381c02f39eadcf4b
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-02-17 17:17:21 +0530

    Time: 1 ms (Beats 87.9%), Space: 44.2 MB (Beats 58.15%)

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,9 +1,32 @@
 class Solution {
 
     public List<List<Integer>> subsets(int[] nums) {
-        return getSubsets(nums, 0, nums.length - 1);
+        
+        //return getSubsets(nums, 0, nums.length - 1);
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), result);
+
+        return result;
     }
 
+
+    // Backtracking is more space optimal
+    private void backtrack(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+
+        result.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(nums, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+
+
+
+
+    // Not an optimal solution, involves keeping List in memory
     private List<List<Integer>> getSubsets(int[] nums, int startIndex, int endIndex) {
 
         if (startIndex > endIndex) {
@@ -11,7 +34,7 @@ class Solution {
             base.add(new ArrayList<>());
             return base;
         }
-        
+
         int currentNumber = nums[startIndex];
         List<List<Integer>> smallAns = getSubsets(nums, startIndex + 1, endIndex);
 
