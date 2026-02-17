45b51be80235885d74e4f43d5546185f54b895d6
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-02-17 16:49:46 +0530

    Time: 1 ms (Beats 87.9%), Space: 44.2 MB (Beats 76.28%)

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -11,12 +11,13 @@ class Solution {
             base.add(new ArrayList<>());
             return base;
         }
+        
         int currentNumber = nums[startIndex];
         List<List<Integer>> smallAns = getSubsets(nums, startIndex + 1, endIndex);
 
         List<List<Integer>> result = new ArrayList<>();
         result.addAll(smallAns);
-        
+
         for (List smallList: smallAns) {
             List<Integer> newList = new ArrayList<>(smallList);
             newList.add(currentNumber);
