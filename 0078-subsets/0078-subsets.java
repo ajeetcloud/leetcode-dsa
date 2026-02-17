787e240ccc20a27cb32cdec6bddef44e6a9f9d9f
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-02-17 16:48:07 +0530

    Time: 1 ms (Beats 87.9%), Space: 44.2 MB (Beats 76.28%)

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        return getSubsets(nums, 0, nums.length - 1);
+    }
+
+    private List<List<Integer>> getSubsets(int[] nums, int startIndex, int endIndex) {
+
+        if (startIndex > endIndex) {
+            List<List<Integer>>base = new ArrayList<>();
+            base.add(new ArrayList<>());
+            return base;
+        }
+        int currentNumber = nums[startIndex];
+        List<List<Integer>> smallAns = getSubsets(nums, startIndex + 1, endIndex);
+        List<List<Integer>> result = new ArrayList<>();
+        result.addAll(smallAns);
+        for (List smallList: smallAns) {
+            List<Integer> newList = new ArrayList<>(smallList);
+            newList.add(currentNumber);
+            result.add(newList);
+        }
+        return result;
+    }
+}
