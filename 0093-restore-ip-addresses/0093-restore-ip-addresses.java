f9201104b814cb3c411daf1dfad6f5cddd6d4043
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-03-11 00:18:40 +0530

    Time: 1 ms (Beats 99.71%), Space: 43.6 MB (Beats 94.9%)

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+
+        List<String> result = new ArrayList<>();
+        int index = 0;
+        int segments = 0;
+        backtrack(result, new StringBuilder(), s, index, segments);
+
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder sb, String s, int index, int segments) {
+
+        if (segments == 4 && index == s.length()) {
+            result.add(sb.toString());
+            return;
+        }
+
+        // This is a nice trick
+        if (segments == 4 || index == s.length()) {
+            return;
+        }
+
+        int num = 0;
+        for (int i = index; i < Math.min(index + 3, s.length()); i++) {
+
+            num = (num * 10 ) + (s.charAt(i) - '0');
+            if (num > 255) {
+                break;
+            }
+            if (s.charAt(index) == '0' && i > index) {
+                break;
+            }
+
+            int origLength = sb.length();
+            sb.append(s, index, i + 1); // Choose
+            if (segments < 3) {
+                sb.append(".");
+            } 
+
+            backtrack(result, sb, s, i + 1, segments + 1); // Recurse
+
+            sb.setLength(origLength);
+        }
+    }
+}
