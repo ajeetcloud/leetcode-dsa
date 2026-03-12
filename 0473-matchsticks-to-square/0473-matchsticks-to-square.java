57acee41a40988c20b7d7ce028554704f63831c0
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-03-12 22:22:35 +0530

    Time: 61 ms (Beats 57.27%), Space: 43.2 MB (Beats 67.98%)

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.java
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -4,14 +4,14 @@ class Solution {
 
         int sum = 0;
         int squareSide = 0;
-        for (int matchstick: matchsticks) {
+        for (int matchstick : matchsticks) {
             sum += matchstick;
         }
         if (sum % 4 != 0) {
             return false;
         }
         squareSide = sum / 4;
-        for (int matchstick: matchsticks) {
+        for (int matchstick : matchsticks) {
             if (matchstick > squareSide) {
                 return false;
             }
@@ -25,7 +25,8 @@ class Solution {
 
     }
 
-    private boolean backtrack(int[] matchsticks, int index, int topSide, int rightSide, int bottomSide, int leftSide, int squareSide) {
+    private boolean backtrack(int[] matchsticks, int index, int topSide, int rightSide, int bottomSide, int leftSide,
+            int squareSide) {
 
         if (index < 0) {
             return topSide == rightSide && rightSide == bottomSide && bottomSide == leftSide && leftSide == squareSide;
@@ -33,20 +34,23 @@ class Solution {
 
         int currentStick = matchsticks[index];
 
-
-        if ((topSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide + currentStick, rightSide, bottomSide, leftSide, squareSide)) {
+        if ((topSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide + currentStick,
+                rightSide, bottomSide, leftSide, squareSide)) {
             return true;
         }
 
-        if ((rightSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide, rightSide + currentStick, bottomSide, leftSide, squareSide)) {
+        if ((rightSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide,
+                rightSide + currentStick, bottomSide, leftSide, squareSide)) {
             return true;
         }
 
-        if ((bottomSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide, rightSide, bottomSide + currentStick, leftSide, squareSide)) {
+        if ((bottomSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide, rightSide,
+                bottomSide + currentStick, leftSide, squareSide)) {
             return true;
         }
 
-        if ((leftSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide, rightSide, bottomSide, leftSide + currentStick, squareSide)) {
+        if ((leftSide + currentStick <= squareSide) && backtrack(matchsticks, index - 1, topSide, rightSide, bottomSide,
+                leftSide + currentStick, squareSide)) {
             return true;
         }
 
