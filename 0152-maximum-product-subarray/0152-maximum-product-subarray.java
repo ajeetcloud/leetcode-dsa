b23114d8b74d0798185672a45b34cd9b0469b3ee
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-03-17 21:11:42 +0530

    Time: 2 ms (Beats 68.52%), Space: 47.1 MB (Beats 83.82%)

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,5 +1,31 @@
 class Solution {
+
+    // Space optimal with constant pace
     public int maxProduct(int[] nums) {
+
+        int n = nums.length;
+        int maxProd = nums[0];
+        int minProd = nums[0];
+
+        int result = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int currentNum = nums[i];
+            int candidate1 = currentNum * maxProd;
+            int candidate2 = currentNum * minProd;
+
+            maxProd = Math.max(currentNum, Math.max(candidate1, candidate2));
+            minProd = Math.min(currentNum, Math.min(candidate1, candidate2));
+
+            result = Math.max(result, maxProd);
+        }
+
+        return result;
+    }
+
+
+    // Not that space optimal
+    public int maxProduct1(int[] nums) {
 
         int n = nums.length;
         int[] maxDp = new int[n];
