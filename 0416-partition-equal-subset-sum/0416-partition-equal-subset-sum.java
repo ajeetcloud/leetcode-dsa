81f22e5e96bd130d518642bfa68f5454e77263dd
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-03-26 23:35:03 +0530

    Time: 61 ms (Beats 38.36%), Space: 47.6 MB (Beats 54.18%)

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -8,10 +8,35 @@ class Solution {
         if (sum % 2 != 0) {
             return false;
         }
-        Arrays.sort(nums);
         int target = sum / 2;
+
+        // DP solution
+        return dp(nums, target);
+
+        /*
+        // Memoization solution
+        Arrays.sort(nums);
         Boolean[][] memo = new Boolean[nums.length][target + 1];
         return bt(nums, 0, target, memo);
+        */
+
+    }
+
+    private boolean dp(int[] nums, int target) {
+
+        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+        dp[nums.length][0] = true; // rest all columns in this row should be 0.
+
+        // recurrence relation
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int j = 0; j <= target; j++) {
+                 dp[i][j] = dp[i + 1][j];
+                if (j >= nums[i] && !dp[i][j]) {
+                    dp[i][j] = dp[i + 1][j - nums[i]];
+                }
+            }
+        }
+        return dp[0][target];
     }
 
     // This is memoization
@@ -31,6 +56,7 @@ class Solution {
             return memo[index][target];
         }
 
+        // (i + 1, j - nums[i]) || (i + 1, j)
         memo[index][target] = bt(nums, index + 1, target - nums[index], memo) || 
                 bt(nums, index + 1, target, memo);
 
