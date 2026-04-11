852b6c490e777729101ff69abc5ecdcef92f1565
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-04-11 21:48:37 +0530

    Time: 0 ms (Beats 100%), Space: 43 MB (Beats 24.72%)

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,6 +1,10 @@
 class Solution {
     public int rob(int[] nums) {
 
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
