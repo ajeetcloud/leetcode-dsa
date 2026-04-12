0e6a44a7efd185ed3fe95de67475645f8a996722
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-04-12 14:31:02 +0530

    Time: 0 ms (Beats 100%), Space: 42.7 MB (Beats 66.22%)

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,30 @@
 class Solution {
-    public int rob(int[] nums) {
-      if (nums.length == 1) {
-        return nums[0];
-      }
-      int lastIndex = nums.length - 1;
-      int ans1 = robSimple(nums, 0, lastIndex - 1);
-      int ans2 = robSimple(nums, 1, lastIndex);
 
-      return Math.max(ans1, ans2);
+    public int rob(int[] nums) {
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        if (nums.length == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int endIndex = nums.length - 1;
+        return Math.max(robLinear(nums, 0, endIndex - 1), robLinear(nums, 1, endIndex));
     }
 
-     public int robSimple(int[] nums, int startIndex, int endIndex) {
-        int storage[] = new int[endIndex - startIndex + 1];
-        if (storage.length == 1) {
-          return nums[startIndex];
-        }
-        storage[0] = nums[startIndex];
-        storage[1] = Math.max(nums[startIndex], nums[startIndex + 1]);
-        for (int i = 2; i < storage.length; i++) {
-          storage[i] = Math.max(storage[i - 1], nums[i + startIndex] + storage[i - 2]);
-        }
-        return storage[storage.length - 1];
-     }
+    private int robLinear(int[] nums, int startIndex, int endIndex) {
 
+        int prevPrev = nums[startIndex];
+        int prev = Math.max(nums[startIndex], nums[startIndex + 1]);
+
+        for (int i = startIndex + 2; i <= endIndex; i++) {
+            
+            int current = Math.max(nums[i] + prevPrev, prev);
+            prevPrev = prev;
+            prev = current;
+        }
+        return prev;
+    }
 }
