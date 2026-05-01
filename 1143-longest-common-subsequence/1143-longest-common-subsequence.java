03c7aa53eb046cbdc39897a5addf999e1ddb3ec5
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-05-01 21:41:11 +0530

    Time: 22 ms (Beats 44.11%), Space: 54.1 MB (Beats 35.03%)

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,12 +1,38 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         
-        Integer[][] memo = new Integer[text1.length()][text2.length()];
-        return memoSolution(text1, text2, 0, 0, memo);
+
+        // DP approach
+        return dpSolution(text1, text2);
 
         /*
+        // Memo approach
+        Integer[][] memo = new Integer[text1.length()][text2.length()];
+        return memoSolution(text1, text2, 0, 0, memo);
+        */
+
+        /*
+        BT approach
         return bt(text1, text2, 0, 0);
         */
+    }
+
+    // DP approach
+    private int dpSolution(String s1, String s2) {
+
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            for (int j = s2.length() - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][ j + 1];
+                } 
+                else {
+                    dp[i][j] = Math.max(dp[i][ j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 
     // Memo approach
