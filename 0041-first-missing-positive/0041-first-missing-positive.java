d42f8d5f91a384bba7b5da16dbee858cf85405e3
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-05-12 19:06:17 +0530

    Time: 1 ms (Beats 100%), Space: 71.1 MB (Beats 71.33%)

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -6,11 +6,9 @@ class Solution {
             int idx = nums[i] - 1;
             if (idx < 0 || idx >= nums.length) {
                 i++;
-            } 
-            else if (nums[i] != nums[idx]) {
+            } else if (nums[i] != nums[idx]) {
                 swap(nums, i, idx);
-            } 
-            else {
+            } else {
                 i++;
             }
         }
