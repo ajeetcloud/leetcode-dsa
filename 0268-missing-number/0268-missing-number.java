ebd223bbcb7c2c9817ed9c1f274f41d700da7579
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-05-13 15:30:38 +0530

    Time: 0 ms (Beats 100%), Space: 47 MB (Beats 90.61%)

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -4,14 +4,14 @@ class Solution {
     public int missingNumber(int[] nums) {
 
         int n = nums.length;
-        int sum = (int) (n * (n + 1) / 2);
+        int expectedSum = (int) (n * (n + 1) / 2);
 
-        int actualSum = 0;
+        int actualSum = 0; // Arrays.stream(nums).sum();
         for (int i = 0; i < nums.length; i++) {
             actualSum += nums[i];
         }
 
-        return sum - actualSum;
+        return expectedSum - actualSum;
     }
 
     // Cyclic Sort
