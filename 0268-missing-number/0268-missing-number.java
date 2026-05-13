e50bb570e99b7ad021b98dd9430fa93c2c265d45
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-05-13 15:15:42 +0530

    Time: 0 ms (Beats 100%), Space: 47.4 MB (Beats 47.97%)

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,21 @@
 class Solution {
 
-    // Cyclic Sort
+    // Math approach
     public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int sum = (int) (n * (n + 1) / 2);
+
+        int actualSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            actualSum += nums[i];
+        }
+
+        return sum - actualSum;
+    }
+
+    // Cyclic Sort
+    public int missingNumber1(int[] nums) {
         
         int i = 0;
         while (i < nums.length) {
