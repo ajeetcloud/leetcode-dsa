ce9cf5a9d7bc70a7fc5dce3198ba994ff01b1bd9
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-05-30 09:28:22 +0530

    Time: 12 ms (Beats 71.65%), Space: 61.6 MB (Beats 9.96%)

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.java
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -1,7 +1,31 @@
 class Solution {
 
-    // This is nlogn solution, less optimal but more intuitive
+    // Stack solution
     public int maxWidthRamp(int[] nums) {
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        // Phase 1 - populating the stack with Indices of decreasing values
+        for (int i = 0; i < nums.length; i++) {
+            if (!stack.isEmpty() && nums[stack.peek()] > nums[i] || stack.isEmpty()) {
+                stack.push(i);
+            } 
+        }
+
+        int maxResult = 0;
+        // Phase 2
+        for (int j = nums.length - 1; j >= 0; j--) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[j]) {
+
+                int left = stack.pop();
+                maxResult = Math.max(maxResult, j - left);
+            }
+        }
+        return maxResult;
+    }
+
+    // This is nlogn solution, less optimal but more intuitive
+    public int maxWidthRamp1(int[] nums) {
 
         Integer[] indices = new Integer[nums.length];
         for (int i = 0; i < nums.length; i++) {
