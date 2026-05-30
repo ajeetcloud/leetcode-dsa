1eb9f606317e732a9c708573a1475b4f53a43573
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-05-30 20:12:17 +0530

    Time: 2 ms (Beats 87.91%), Space: 43.6 MB (Beats 69.76%)

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        
+        // Monotonic stack -> a, b, c(top)
+        Deque<Character> stack = new ArrayDeque<>();
+        int[] count = new int[26];
+
+        boolean[] inStack = new boolean[26];
+
+        for (char c: s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c: s.toCharArray()) {
+
+            count[c - 'a']--;
+            if (inStack[c - 'a']) {
+                continue;
+            }
+            char currentChar = c;
+            while (!stack.isEmpty() && stack.peek() > c && count[stack.peek() - 'a'] > 0) {
+
+                inStack[stack.peek() - 'a'] = false;
+                stack.pop();
+            }
+            stack.push(c);
+            inStack[c - 'a'] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+        
+    }
+}
