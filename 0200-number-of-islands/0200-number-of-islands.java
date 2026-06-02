c07ecf89464ec2290fa1e46365ce2444b5cd8c1a
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-06-02 08:14:07 +0530

    Time: 4 ms (Beats 44.11%), Space: 52.3 MB (Beats 59.21%)

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,12 +1,12 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        
+
         int m = grid.length;
         int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
 
         int count = 0;
-        
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
@@ -19,7 +19,7 @@ class Solution {
         return count;
     }
 
-    private void explore (char[][] grid, int i, int j, boolean[][] visited) {
+    private void explore(char[][] grid, int i, int j, boolean[][] visited) {
 
         int m = grid.length;
         int n = grid[0].length;
