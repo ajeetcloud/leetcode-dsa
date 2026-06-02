706e32fc5e2cef82d4d476399a43e9bd62e6da55
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-06-02 16:19:14 +0530

    Time: 6 ms (Beats 20.79%), Space: 51.6 MB (Beats 84.19%)

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,5 +1,63 @@
+record Location(int i, int j) {
+}
+
 class Solution {
+
+    private static final int[][] DIRECTIONS = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+    // BFS
     public int numIslands(char[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        Deque<Location> queue = new ArrayDeque<>();
+
+        int islands = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    islands++;
+                    bfs(grid, i, j, visited);
+                }
+            }
+        }
+        return islands;
+    }
+
+    private void bfs(char[][] grid, int startRow, int startCol, boolean[][] visited) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Deque<Location> queue = new ArrayDeque<>();
+        queue.offer(new Location(startRow, startCol));
+        visited[startRow][startCol] = true; // Mark on Enqueue!
+
+        while (!queue.isEmpty()) {
+            Location loc = queue.poll();
+
+            for (int[] dir : DIRECTIONS) {
+                int nextRow = loc.i() + dir[0];
+                int nextCol = loc.j() + dir[1];
+
+                if (nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= cols || visited[nextRow][nextCol]) {
+                    continue;
+                }
+
+                if (grid[nextRow][nextCol] == '1') {
+                    queue.offer(new Location(nextRow, nextCol));
+                    visited[nextRow][nextCol] = true;
+                }
+
+            }
+        }
+    }
+
+    // Recursive DFS
+    public int numIslandsDFS(char[][] grid) {
 
         int m = grid.length;
         int n = grid[0].length;
@@ -11,7 +69,7 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
                     count++;
-                    explore(grid, i, j, visited);
+                    dfs(grid, i, j, visited);
                 }
             }
         }
@@ -19,7 +77,7 @@ class Solution {
         return count;
     }
 
-    private void explore(char[][] grid, int i, int j, boolean[][] visited) {
+    private void dfs(char[][] grid, int i, int j, boolean[][] visited) {
 
         int m = grid.length;
         int n = grid[0].length;
@@ -29,9 +87,9 @@ class Solution {
         }
         visited[i][j] = true;
 
-        explore(grid, i, j + 1, visited);
-        explore(grid, i, j - 1, visited);
-        explore(grid, i + 1, j, visited);
-        explore(grid, i - 1, j, visited);
+        dfs(grid, i, j + 1, visited);
+        dfs(grid, i, j - 1, visited);
+        dfs(grid, i + 1, j, visited);
+        dfs(grid, i - 1, j, visited);
     }
 }
