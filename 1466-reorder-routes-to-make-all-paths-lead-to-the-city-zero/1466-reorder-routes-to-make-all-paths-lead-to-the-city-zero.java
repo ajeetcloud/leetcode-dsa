aa983c249a8392a59b594aa775dd0d068bba9f3a
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-06-09 14:19:09 +0530

    Time: 57 ms (Beats 38.89%), Space: 124 MB (Beats 35.25%)

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,5 +1,6 @@
 class Solution {
 
+    // BFS approach
     public int minReorder(int n, int[][] connections) {
 
         List<List<Integer>> adj = new ArrayList<>();
@@ -10,7 +11,52 @@ class Solution {
         // If we create a String set we can store like - '0->1'
         Set<Integer> origRoute = new HashSet<>();
 
-        for (int[] connection: connections) {
+        for (int[] connection : connections) {
+            int from = connection[0];
+            int to = connection[1];
+            // origRoute.add(from + "->" + to);
+            origRoute.add(from * n + to);
+            adj.get(from).add(to);
+            adj.get(to).add(from);
+        }
+
+        boolean[] visited = new boolean[n];
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(0);
+        visited[0] = true;
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            List<Integer> neighbors = adj.get(current);
+            for (Integer neighbor : neighbors) {
+                if (visited[neighbor]) {
+                    continue;
+                }
+                queue.offer(neighbor);
+                visited[neighbor] = true;
+                int edge = (current * n) + neighbor;
+                if (origRoute.contains(edge)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int minReorderDFS(int n, int[][] connections) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // If we create a String set we can store like - '0->1'
+        Set<Integer> origRoute = new HashSet<>();
+
+        for (int[] connection : connections) {
             int from = connection[0];
             int to = connection[1];
             // origRoute.add(from + "->" + to);
@@ -30,7 +76,7 @@ class Solution {
         int count = 0;
         List<Integer> neighbors = adj.get(originNode);
 
-        for (Integer neighbor: neighbors) {
+        for (Integer neighbor : neighbors) {
             if (visited[neighbor]) {
                 continue;
             }
