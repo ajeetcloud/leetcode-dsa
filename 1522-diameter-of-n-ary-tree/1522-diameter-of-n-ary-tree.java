dd61544472979b1ebe91c1fccbaaf5f79703b13e
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-06-20 20:55:38 +0530

    Time: 0 ms (Beats 100%), Space: 46.2 MB (Beats 42.31%)

---
+++ 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.java
@@ -0,0 +1,86 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+
+    int diameter = 0;
+
+    public int diameter(Node root) {
+
+        dfs(root);
+        return diameter;
+    }
+
+    private int dfs(Node root) {
+
+        int best1 = 0;
+        int best2 = 0;
+
+        if (root == null) {
+            return 0;
+        }
+
+        List<Node> children = root.children;
+
+        for (Node child: children) {
+
+            int childHeight = dfs(child);
+            if (childHeight > best1) {
+                best2 = best1;
+                best1 = childHeight;
+            }
+            else if (childHeight > best2) {
+                best2 = childHeight;
+            }
+        }
+
+        diameter = Math.max(diameter, (best1 + best2));
+
+        return 1 + best1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
