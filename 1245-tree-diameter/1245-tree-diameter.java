6703a124014d2ad1e19cb6eccf260be0d981b9c6
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-06-20 20:16:39 +0530

    Time: 10 ms (Beats 77.31%), Space: 47.3 MB (Beats 98.32%)

--- 1245-tree-diameter/1245-tree-diameter.java
+++ 1245-tree-diameter/1245-tree-diameter.java
@@ -3,7 +3,52 @@ class Solution {
     int diameter = 0;
     List<List<Integer>> adj;
 
+    int maxDist;
+    int farthestNode;
+
     public int treeDiameter(int[][] edges) {
+
+        int n = edges.length + 1;
+        adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge: edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        // 1st pass. find the farthest node from start node
+        dfsEdgetoEdge(0, -1, 0);
+        int farthestNodeOfDiameter = farthestNode;
+
+        dfsEdgetoEdge(farthestNodeOfDiameter, -1, 0);
+
+        return maxDist;
+    }
+
+    private void dfsEdgetoEdge(int node, int parent, int dist) {
+
+        if (dist > maxDist) {
+            maxDist = dist;
+            farthestNode = node;
+        }
+
+        List<Integer> neighbors = adj.get(node);
+        for (int neighbor: neighbors) {
+            if (neighbor != parent) {
+                dfsEdgetoEdge(neighbor, node, dist + 1);
+            }   
+        }
+    }
+
+
+
+    //=======================================================================================
+
+    public int treeDiameterDFS(int[][] edges) {
 
         int n = edges.length + 1;
         adj = new ArrayList<>();
@@ -21,6 +66,7 @@ class Solution {
         return diameter;
     }
 
+    // Calculate best 2 heights
     private int heightDfs(int node, int parent) {
         
         int best1 = 0;
