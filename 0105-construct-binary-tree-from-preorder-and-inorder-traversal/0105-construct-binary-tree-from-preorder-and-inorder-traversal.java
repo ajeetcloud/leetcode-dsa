fbf6372e8f07e1d34751968039f9f38283789972
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-06-29 21:59:40 +0530

    Time: 3 ms (Beats 30.42%), Space: 45.2 MB (Beats 92.22%)

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        int length = preorder.length;
+        return dfs(preorder, inorder, 0, length - 1, 0, length - 1);
+    }
+
+private TreeNode dfs(int[] preorder, int[] inorder, int startIndexPre, int endIndexPre, int startIndexIn,
+int endIndexIn) {
+
+    if (startIndexPre > endIndexPre || startIndexIn > endIndexIn) {
+        return null;
+    }
+
+    TreeNode root = new TreeNode(preorder[startIndexPre]);
+    int inOrderRootIndex = -1;
+    int elementsInLeftSubTree = 0;
+    for (int i = startIndexIn; i <= endIndexIn; i++) {
+        if (inorder[i] == root.val) {
+            inOrderRootIndex = i;
+            break;
+        }
+        elementsInLeftSubTree++;
+    }
+
+    root.left = dfs(preorder, inorder, startIndexPre + 1, startIndexPre + elementsInLeftSubTree, startIndexIn, inOrderRootIndex - 1);
+    root.right = dfs(preorder, inorder, startIndexPre + elementsInLeftSubTree + 1, endIndexPre, inOrderRootIndex + 1, endIndexIn);
+
+    return root;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
