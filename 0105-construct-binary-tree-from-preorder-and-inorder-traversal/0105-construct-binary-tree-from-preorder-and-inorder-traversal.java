06ed73e87afa1bd8f3b1ec48a7e7f7c7120967c5
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-06-30 22:11:59 +0530

    Time: 2 ms (Beats 64.05%), Space: 46.4 MB (Beats 36.63%)

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -18,7 +18,7 @@ class Solution {
     Map<Integer, Integer> inOrderMap = new HashMap<>();
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
+
         int length = preorder.length;
         for (int i = 0; i < length; i++) {
             inOrderMap.put(inorder[i], i);
@@ -26,50 +26,32 @@ class Solution {
         return dfs(preorder, inorder, 0, length - 1, 0, length - 1);
     }
 
-private TreeNode dfs(int[] preorder, int[] inorder, int startIndexPre, int endIndexPre, int startIndexIn,
-int endIndexIn) {
+    private TreeNode dfs(int[] preorder, int[] inorder, int startIndexPre, int endIndexPre, int startIndexIn,
+            int endIndexIn) {
 
-    if (startIndexPre > endIndexPre || startIndexIn > endIndexIn) {
-        return null;
-    }
+        if (startIndexPre > endIndexPre || startIndexIn > endIndexIn) {
+            return null;
+        }
 
-    TreeNode root = new TreeNode(preorder[startIndexPre]);
-    int inOrderRootIndex = inOrderMap.get(root.val);
-    int elementsInLeftSubTree = inOrderRootIndex - startIndexIn;
-    /*
-    // For loop makes it quadratic time complexity, using map brings it back to linear
-    for (int i = startIndexIn; i <= endIndexIn; i++) {
+        TreeNode root = new TreeNode(preorder[startIndexPre]);
+        int inOrderRootIndex = inOrderMap.get(root.val);
+        int elementsInLeftSubTree = inOrderRootIndex - startIndexIn;
+        /*
+        // For loop makes it quadratic time complexity, using map brings it back to linear
+        for (int i = startIndexIn; i <= endIndexIn; i++) {
         if (inorder[i] == root.val) {
             inOrderRootIndex = i;
             break;
         }
         elementsInLeftSubTree++;
+        }
+        */
+
+        root.left = dfs(preorder, inorder, startIndexPre + 1, startIndexPre + elementsInLeftSubTree, startIndexIn,
+                inOrderRootIndex - 1);
+        root.right = dfs(preorder, inorder, startIndexPre + elementsInLeftSubTree + 1, endIndexPre,
+                inOrderRootIndex + 1, endIndexIn);
+
+        return root;
     }
-    */
-
-    root.left = dfs(preorder, inorder, startIndexPre + 1, startIndexPre + elementsInLeftSubTree, startIndexIn, inOrderRootIndex - 1);
-    root.right = dfs(preorder, inorder, startIndexPre + elementsInLeftSubTree + 1, endIndexPre, inOrderRootIndex + 1, endIndexIn);
-
-    return root;
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
