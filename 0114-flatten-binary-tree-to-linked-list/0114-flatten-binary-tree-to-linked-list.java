22ddead4f9feb541fb6054b126259c2a91f2ba2e
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-07-04 21:23:54 +0530

    Time: 0 ms (Beats 100%), Space: 44.2 MB (Beats 15.68%)

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -17,7 +17,25 @@ class Solution {
 
     public void flatten(TreeNode root) {
 
+        // Morris Traversal
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode prev = curr.left;
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+
+        /*
+        // DFS Recursive approach    
         dfsFlatten(root);
+        */
     }
 
     private TreeNode dfsFlatten(TreeNode node) {
