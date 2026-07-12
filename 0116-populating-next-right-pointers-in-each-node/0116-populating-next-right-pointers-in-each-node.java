352a128316d339c1b17066cc3f7e1ab8802619bb
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-07-12 17:15:02 +0530

    Time: 3 ms (Beats 35.71%), Space: 46.6 MB (Beats 17.55%)

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,70 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+
+        if (root == null) {
+            return null;
+        }
+        
+            Queue<Node> queue = new ArrayDeque<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+                Node prev = null; // every level starts fresh
+
+                for (int i = 0; i < levelSize; i++) {
+                    // visit
+                    Node node = queue.poll();
+                    if (prev != null) {
+                        prev.next = node;
+                    }
+                    prev = node;
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+
+            }
+
+        return root;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
