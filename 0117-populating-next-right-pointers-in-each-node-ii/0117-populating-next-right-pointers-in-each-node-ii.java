c3cd64a9468bf911e548c2b1aea0fe74f8685de7
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2026-07-15 21:58:18 +0530

    Time: 0 ms (Beats 100%), Space: 46.2 MB (Beats 76.36%)

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        
+        Node leftmost = root;
+
+        while (leftmost != null) {
+
+            Node dummy = new Node();
+            Node tail = dummy;
+            Node head = leftmost;
+
+            while (head != null) {
+                if (head.left != null) {
+                    tail.next = head.left;
+                    tail = tail.next;
+                }
+                if (head.right != null) {
+                    tail.next = head.right;
+                    tail = tail.next;
+                }
+                head = head.next;
+            }
+            leftmost = dummy.next;
+        }
+        return root;
+    }
+}
+
+
+
+
+
+
+
+
+
